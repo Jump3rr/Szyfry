b055fe0a8afebdca75a90931d847033e54c59891
Jump3rr <theneonxd@gmail.com>
2021-01-23 15:39:21 +0100

    naprawiono szyfr vigenera - dodano odszyfrowanie

--- Vigener/main.js
+++ Vigener/main.js
@@ -2,38 +2,18 @@ document.querySelector('#szyfruj').addEventListener('click',szyfruj);
 const pokazTekst = document.querySelector('#szyfr');
 const alfabet = ['A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','Q','R','S','Ś','T','U','V','W','X','Y','Z','Ź','Ż'];
 //const alfabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+document.querySelector('#odszyfruj').addEventListener('click',odszyfruj);
+const HTMLNode = document.createElement('section');
+const HTMLTekst = document.createElement('h1');
 
-function szyfruj() {
-    let znaki = [];
-    let kluczZnaki = [];
+function szyfruj(kluczZnaki) {
+    if(typeof(kluczZnaki)!=='string')
+        kluczZnaki = odczytajKlucz();
+    else
+        kluczZnaki=odczytajKlucz(kluczZnaki);
+
+    let znaki = odczytajTekst();
     let zaszyfrowany = [];
-    const tekst = (document.getElementById('tekst').value).toUpperCase();
-    const klucz = (document.getElementById('klucz').value).toUpperCase();
-    const HTMLNode = document.createElement('section');
-    const HTMLTekst = document.createElement('h1');
-    let i=0;
-    klucz.split('')
-        .forEach(element => { 
-            for(let j=0; j<alfabet.length;j++) {
-                if(element==alfabet[j]) {
-                    kluczZnaki[i] = element;
-                    i++;
-                }
-            }
-        });
-    i=0;
-    tekst.split('')
-        .forEach(element => { 
-            for(let j=0; j<alfabet.length;j++) {
-                if(element==alfabet[j]) {
-                    znaki[i] = element;
-                    i++;
-                }
-            }
-    });
-    console.log(kluczZnaki);
-    console.log(znaki);
-    console.log(zaszyfrowany);
     let a=0;
     let b=0;
     for(let i=0; i<znaki.length; i++) {
@@ -70,3 +50,75 @@ function szyfruj() {
     pokazTekst.appendChild(HTMLNode); 
 
 }
+function odszyfruj() {
+    let kluczZnaki = odczytajKlucz();
+    let znaki = odczytajTekst();
+    let zaszyfrowany = [];
+    let a=0;
+    let b=0;
+    for(let i=0; i<kluczZnaki.length; i++) {
+        for(let j=0; j<alfabet.length; j++) {
+            if(zaszyfrowany[i]==undefined) {
+                if(kluczZnaki[a]==alfabet[j]) {
+                    for(let k=0; k<znaki.length; k++) {
+                        if(zaszyfrowany[i]==undefined) {
+                            for(let m=0; m<alfabet.length; m++) {
+                                if(znaki[b]==alfabet[m]) {
+                                if(j+m>=alfabet.length)
+                                    zaszyfrowany[i] = alfabet[(alfabet.length-j)%alfabet.length];
+                                else if(j+m<0)
+                                    zaszyfrowany[i] = alfabet[(alfabet.length-j)%alfabet.length];
+                                else
+                                    zaszyfrowany[i] = alfabet[(alfabet.length-j)%alfabet.length];
+                                break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        a++;
+        b++;
+        if(a>=kluczZnaki.length)
+            a=0; 
+            
+    let odwroconyKlucz = zaszyfrowany.join("");
+    HTMLTekst.innerHTML = odwroconyKlucz;
+    szyfruj(odwroconyKlucz);
+    }
+    
+}
+
+function odczytajKlucz(klucz = (document.getElementById('klucz').value).toUpperCase()) {
+    let kluczZnaki = [];
+    let i=0;
+    klucz.split('')
+        .forEach(element => { 
+            for(let j=0; j<alfabet.length;j++) {
+                if(element==alfabet[j]) {
+                    kluczZnaki[i] = element;
+                    i++;
+                }
+            }
+        });
+
+    return kluczZnaki;
+}
+function odczytajTekst() {
+    const tekst = (document.getElementById('tekst').value).toUpperCase();
+    let znaki = [];
+
+    let i=0;
+    tekst.split('')
+        .forEach(element => { 
+            for(let j=0; j<alfabet.length;j++) {
+                if(element==alfabet[j]) {
+                    znaki[i] = element;
+                    i++;
+                }
+            }
+    });
+    return znaki;
+}
+
