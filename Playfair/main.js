7e0855cf303866fc1847b6fa3e80c207308b796b
Jump3rr <theneonxd@gmail.com>
2021-01-29 00:07:30 +0100

    szyfr playfair - complete

--- Playfair/main.js
+++ Playfair/main.js
@@ -1,4 +1,5 @@
 document.querySelector('#szyfruj').addEventListener('click',szyfruj);
+document.querySelector('#odszyfruj').addEventListener('click',odszyfruj);
 const pokazTekst = document.querySelector('#szyfr');
 const alfabet = ['A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','Q','R','S','Ś','T','U','V','W','X','Y','Z','Ź','Ż'];
 //const alfabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -13,11 +14,20 @@ const tablica = [
     ['', '', '', '', ''],
     ['', '', '', '', '']
 ];
-console.log(tablica);
 
 function szyfruj() {
     let kluczZnaki = odczytajKlucz();
     let check = 0;
+    let znaki = odczytajTekst();
+
+    if((znaki.length%2)==1) {
+        if(znaki[znaki.length-1]!='X')
+            znaki[znaki.length] = 'X';
+        else
+            znaki[znaki.length] = 'Z';
+    }
+    let wynik = [];
+
     for(let i=0; i<kluczZnaki.length;i++) {
         for(let j=0; j<7; j++) {
             for(let k=0; k<5; k++) {
@@ -49,7 +59,162 @@ function szyfruj() {
     }
 
 
+    let wierszP = [];
+    let kolumnaP = [];
+    for(let a=0; a<=znaki.length;a++) {
+        for(let i=0; i<7; i++) {
+            for(let j=0; j<5; j++) {
+                if(znaki[a]==tablica[i][j]) {
+                    wierszP[a] = i;
+                    kolumnaP[a] = j; 
+                }
+            }
+        }
+    }
+
+    for(let pierwszy = 0; pierwszy<znaki.length;pierwszy=pierwszy+2) {
+        if(wierszP[pierwszy] == wierszP[pierwszy+1] && kolumnaP[pierwszy] == kolumnaP[pierwszy+1]) {
+            wierszP[pierwszy+1] = 6;
+            kolumnaP[pierwszy+1] = 1;
+        }
+
+        if(wierszP[pierwszy] == wierszP[pierwszy+1]) {
+            if(kolumnaP[pierwszy] < 4)
+                wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy]+1];
+            else
+                wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy]-4];
+
+            if(kolumnaP[pierwszy+1] < 4)
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy+1]+1];
+            else
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy+1]-4];
+            
+        }
+        else if(kolumnaP[pierwszy] == kolumnaP[pierwszy+1]) {
+            if(wierszP[pierwszy] < 6)
+                wynik[pierwszy] = tablica[wierszP[pierwszy]+1][kolumnaP[pierwszy]];
+            else
+                wynik[pierwszy] = tablica[wierszP[pierwszy]-6][kolumnaP[pierwszy]];
+
+            if(wierszP[pierwszy+1] < 6)
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]+1][kolumnaP[pierwszy+1]];
+            else
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]-6][kolumnaP[pierwszy+1]];
+
+        }
+        else {
+            wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy+1]];
+            wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy]];
+
+        }
+    }
+    console.log(tablica);
+    let xx = wynik.join("");
+    HTMLTekst.innerHTML = xx;
+    HTMLNode.appendChild(HTMLTekst);
+    pokazTekst.appendChild(HTMLNode); 
+
 }
+
+function odszyfruj() {
+    let kluczZnaki = odczytajKlucz();
+    let check = 0;
+    let znaki = odczytajTekst();
+
+    if((znaki.length%2)==1) {
+        if(znaki[znaki.length-1]!='X')
+            znaki[znaki.length] = 'X';
+        else
+            znaki[znaki.length] = 'Z';
+    }
+    let wynik = [];
+
+    for(let i=0; i<kluczZnaki.length;i++) {
+        for(let j=0; j<7; j++) {
+            for(let k=0; k<5; k++) {
+                if(kluczZnaki[i]!=tablica[j][k])
+                    check++;
+            }
+        }
+        if(check==35) {
+            zamien(i);
+            check=0;
+        }
+        else
+            check=0;
+    }
+
+    for(let i=0; i<alfabet.length;i++) {
+        for(let j=0; j<7; j++) {
+            for(let k=0; k<5; k++) {
+                if(alfabet[i]!=tablica[j][k])
+                    check++;
+            }
+        }
+        if(check==35) {
+            zamienAlfabet(i);
+            check=0;
+        }
+        else
+            check=0;
+    }
+
+
+    let wierszP = [];
+    let kolumnaP = [];
+    for(let a=0; a<=znaki.length;a++) {
+        for(let i=0; i<7; i++) {
+            for(let j=0; j<5; j++) {
+                if(znaki[a]==tablica[i][j]) {
+                    wierszP[a] = i;
+                    kolumnaP[a] = j; 
+                }
+            }
+        }
+    }
+
+    for(let pierwszy = 0; pierwszy<znaki.length;pierwszy=pierwszy+2) {
+
+        if(wierszP[pierwszy] == wierszP[pierwszy+1]) {
+            if(kolumnaP[pierwszy] > 0)
+                wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy]-1];
+            else
+                wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy]+4];
+
+            if(kolumnaP[pierwszy+1] > 0)
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy+1]-1];
+            else
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy+1]+4];
+            
+        }
+        else if(kolumnaP[pierwszy] == kolumnaP[pierwszy+1]) {
+            if(wierszP[pierwszy] > 0)
+                wynik[pierwszy] = tablica[wierszP[pierwszy]-1][kolumnaP[pierwszy]];
+            else
+                wynik[pierwszy] = tablica[wierszP[pierwszy]+6][kolumnaP[pierwszy]];
+
+            if(wierszP[pierwszy+1] > 0)
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]-1][kolumnaP[pierwszy+1]];
+            else
+                wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]+6][kolumnaP[pierwszy+1]];
+
+        }
+        else {
+            wynik[pierwszy] = tablica[wierszP[pierwszy]][kolumnaP[pierwszy+1]];
+            wynik[pierwszy+1] = tablica[wierszP[pierwszy+1]][kolumnaP[pierwszy]];
+
+        }
+        if(wynik[pierwszy+1]=='X') {
+            wynik[pierwszy+1]=wynik[pierwszy];
+        }
+    }
+    let xx = wynik.join("");
+    HTMLTekst.innerHTML = xx;
+    HTMLNode.appendChild(HTMLTekst);
+    pokazTekst.appendChild(HTMLNode); 
+
+}
+
 function zamien(i) {
     let kluczZnaki = odczytajKlucz();
     for(let j=0; j<7; j++) {
@@ -62,7 +227,6 @@ function zamien(i) {
                 break;
     }
     }
-    console.log(tablica);
 }
 function zamienAlfabet(i) {
     for(let j=0; j<7; j++) {
@@ -75,7 +239,6 @@ function zamienAlfabet(i) {
                 break;
     }
     }
-    console.log(tablica);
 }
 
 function odczytajKlucz() {
@@ -92,5 +255,30 @@ function odczytajKlucz() {
             }
         });
 
-    return kluczZnaki;
+    var uniqueArray = [];
+
+    for(i=0; i < kluczZnaki.length; i++){
+        if(uniqueArray.indexOf(kluczZnaki[i]) === -1) {
+            uniqueArray.push(kluczZnaki[i]);
+        }
+    }
+
+    return uniqueArray;
+}
+
+function odczytajTekst() {
+    const tekst = (document.getElementById('tekst').value).toUpperCase();
+    let znaki = [];
+
+    let i=0;
+    tekst.split('')
+        .forEach(element => { 
+            for(let j=0; j<alfabet.length;j++) {
+                if(element==alfabet[j]) {
+                    znaki[i] = element;
+                    i++;
+                }
+            }
+    });
+    return znaki;
 }
